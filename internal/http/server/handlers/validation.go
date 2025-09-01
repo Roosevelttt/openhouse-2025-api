9c86ade8c1adac9e3694f135868fb84d9ae0dd3d
ChristopherJ123 <christopherz2002@hotmail.com>
2025-09-01 22:37:36 +0700

    feat: validasi atau reject pembayaran hanya bisa oleh ketua UKM, divisi IT, atau BPH

--- internal/http/server/handlers/validation.go
+++ internal/http/server/handlers/validation.go
@@ -43,7 +43,7 @@ func (h *PaymentHandler) Validate(c *gin.Context) {
 	// Klo bukan BPH atau IT atau ketua UKM maka tidak boleh melakukan validasi
 	adminDivisionSlug, _ := c.Get("admin_division_slug")
 	if adminDivisionSlug != "bph" && adminDivisionSlug != "it" && adminUkmID != req.UKM {
-		c.JSON(http.StatusUnauthorized, gin.H{"error": "Validasi hanya bisa dilakukan oleh BPH, IT dan Ketua UKM"})
+		c.JSON(http.StatusUnauthorized, gin.H{"message": "Validasi hanya bisa dilakukan oleh BPH, IT dan Ketua UKM"})
 		return
 	}
 
@@ -95,6 +95,13 @@ func (h *PaymentHandler) Reject(c *gin.Context) {
 	}
 	adminName, _ := c.Get("admin_name")
 	adminUkmID, _ := c.Get("admin_ukm_id")
+
+	// Klo bukan BPH atau IT atau ketua UKM maka tidak boleh melakukan validasi
+	adminDivisionSlug, _ := c.Get("admin_division_slug")
+	if adminDivisionSlug != "bph" && adminDivisionSlug != "it" && adminUkmID != req.UKM {
+		c.JSON(http.StatusUnauthorized, gin.H{"message": "Validasi hanya bisa dilakukan oleh BPH, IT dan Ketua UKM"})
+		return
+	}
 
 	adminCtx := services.AdminContext{
 		NRP:   adminNRP.(string),
