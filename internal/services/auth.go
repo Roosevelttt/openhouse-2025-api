f5e95888b6e8c877f4359dfb653aef5c65421d01
Kelly <88363184+Waxomoly@users.noreply.github.com>
2025-09-04 12:40:10 +0700

    feat: success login message

--- internal/services/auth.go
+++ internal/services/auth.go
@@ -122,7 +122,7 @@ func (s *AuthService) OAuthCallback(c *gin.Context) {
 	// c.JSON(http.StatusAccepted, sessionData)
 
 	// Handle redirect after login
-	redirectURL := os.Getenv("CORS_ORIGINS")
+	redirectURL := os.Getenv("CORS_ORIGINS") + "?success=success_login"
 
 	// If user is admin, check if there was a stored redirect intention
 	if admin != nil {
