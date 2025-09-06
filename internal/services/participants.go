28ec024e5c855d64e0a8da9968b21d75750b03af
Justin <andreasjustin3020@gmail.com>
2025-09-07 02:04:16 +0700

    feat: fitur email konfirmasi pendaftaran

--- internal/services/participants.go
+++ internal/services/participants.go
@@ -10,12 +10,24 @@ import (
 type ParticipantsService struct {
 	participants *repositories.ParticipantsRepository
 	registration *repositories.RegistrationRepository
+	mailService  *MailService
+	userRepo     *repositories.UserRepository
+	ukmRepo      *repositories.UkmRepository
 }
 
-func NewParticipantsService(repo *repositories.ParticipantsRepository, regRepo *repositories.RegistrationRepository) *ParticipantsService {
+func NewParticipantsService(
+	repo *repositories.ParticipantsRepository,
+	regRepo *repositories.RegistrationRepository,
+	mailService *MailService,
+	userRepo *repositories.UserRepository,
+	ukmRepo *repositories.UkmRepository,
+) *ParticipantsService {
 	return &ParticipantsService{
 		participants: repo,
 		registration: regRepo,
+		mailService:  mailService,
+		userRepo:     userRepo,
+		ukmRepo:      ukmRepo,
 	}
 }
 
@@ -42,7 +54,52 @@ func (s *ParticipantsService) ReserveSlot(ctx context.Context, nrp, ukmID string
 
 // RegisterWithReservation creates a registration using an existing reservation
 func (s *ParticipantsService) RegisterWithReservation(ctx context.Context, reservationID string, reg *models.DetailRegistration) error {
-	return s.registration.ConsumeReservation(ctx, reservationID, reg)
+	// First, complete the registration
+	err := s.registration.ConsumeReservation(ctx, reservationID, reg)
+	if err != nil {
+		return err
+	}
+
+	// Send email notifications if mail service is configured
+	if s.mailService != nil {
+		// Get user details
+		user, err := s.userRepo.FindByNRP(ctx, reg.NRP)
+		if err != nil {
+			// Log error but don't fail the registration
+			fmt.Printf("Warning: Could not get user details for email: %v\n", err)
+			return nil
+		}
+
+		// Get UKM details
+		ukm, err := s.ukmRepo.FindByID(ctx, reg.UkmID)
+		if err != nil {
+			// Log error but don't fail the registration
+			fmt.Printf("Warning: Could not get UKM details for email: %v\n", err)
+			return nil
+		}
+
+		// --- Email Sending Logic (using payment confirmation template) ---
+		fmt.Printf("INFO: Sending payment confirmation email to %s for UKM %s\n", user.NRP, ukm.Name)
+
+		// Prepare payment confirmation data
+		emailData := PaymentConfirmationData{
+			UserName: user.Name,
+			UserNRP:  user.NRP,
+			UkmName:  ukm.Name,
+		}
+
+		// Use SendPaymentConfirmationEmailTemplate for payment submissions
+		go func() {
+			err := s.mailService.SendPaymentConfirmationEmailTemplate(user.NRP, emailData)
+			if err != nil {
+				fmt.Printf("ERROR: Failed to send payment confirmation email to %s: %v\n", user.NRP, err)
+			} else {
+				fmt.Printf("INFO: Payment confirmation email sent successfully to %s\n", user.NRP)
+			}
+		}()
+	}
+
+	return nil
 }
 
 // CheckUserReservation checks if user has a valid reservation for a UKM
